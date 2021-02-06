d52a3f25f4d288afcaf1043c6b8e1be86b0ed790
Dragos Rachieru <rachierudragos97@gmail.com>
2021-02-06 19:16:06 +0200

    Updated build gradle for ios

--- payload/build.gradle.kts
+++ payload/build.gradle.kts
@@ -8,13 +8,15 @@ plugins {
 version = Versions.app
 
 kotlin {
-    /* Targets configuration omitted. 
-    *  To find out how to configure the targets, please follow the link:
-    *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
+
     jvm()
     js(BOTH) {
         nodejs()
-//        browser()
+    }
+    ios {
+        binaries.framework {
+            baseName = "SevensPayload"
+        }
     }
 
     sourceSets {
